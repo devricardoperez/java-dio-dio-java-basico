1a0c34a3d73a91a8fa2b3355135396ad6100af92
devricardoperez <ricardoperezzeller@gmail.com>
2024-09-26 20:08:46 -0300

    Java Anatomia Clases

---
+++ java-anatomia-clases/src/MinhaClasse.java
@@ -0,0 +1,16 @@
+public class MinhaClasse {
+
+
+public static void main(String[] args) {
+
+    System.out.println("Ola turma, seja bem-vindos");
+    int ano = 2022;
+    ano = 2024;
+
+    final Integer MES_09 = 9;
+    //Pi es final e outros
+
+
+
+    }
+}
